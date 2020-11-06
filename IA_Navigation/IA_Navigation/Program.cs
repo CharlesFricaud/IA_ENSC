5239cc12c9c830bd9837e51f8a92eac78a48e2e9
Loïc <56264074+lmedina002@users.noreply.github.com>
2020-11-06 14:24:03 +0100

    Creation

---
+++ IA_Navigation/IA_Navigation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IA_Navigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
