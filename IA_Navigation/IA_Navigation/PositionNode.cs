3845c7ea8a67c4d0ff84daac987f336fb6653609
CharlesF <cfricaud001@ensc.fr>
2020-11-12 09:35:16 +0100

    Hcost avec vol d'oiseau

--- IA_Navigation/IA_Navigation/PositionNode.cs
+++ IA_Navigation/IA_Navigation/PositionNode.cs
@@ -66,7 +66,7 @@ namespace IA_Navigation
 
         public override double CalculeHCost()
         {
-            return 0;
+            return Math.Sqrt((x - xf) * (x - xf) + (y - yf) * (y - yf));
         }
 
         public override string ToString()
